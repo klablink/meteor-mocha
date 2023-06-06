5d4e8b9a7dc88b68e7e446bd1e69cf7a8834ec37
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-06-06 11:14:03 -0300

    updated to handle 3 >

--- package/package.js
+++ package/package.js
@@ -1,19 +1,19 @@
 Package.describe({
-  name: 'meteortesting:mocha',
+  name: 'grubba:mocha',
   summary: 'Run Meteor package or app tests with Mocha',
   git: 'https://github.com/meteortesting/meteor-mocha.git',
   documentation: '../README.md',
-  version: '3.0.0',
+  version: '3.0.1-alpha300.9',
   testOnly: true,
 });
 
 Package.onUse(function onUse(api) {
   api.use([
-    'meteortesting:mocha-core@8.2.1',
-    'ecmascript@0.15.1',
+    'denyhs:mocha-core@8.2.2-alpha300.9',
+    'ecmascript@1.0.0-alpha300.9',
   ]);
 
-  api.use(['meteortesting:browser-tests@1.4.2', 'http@1.0.0 || 2.0.0'], 'server');
+  api.use(['grubba:browser-tests@1.5.2-alpha300.9', 'http@1.0.0 || 2.0.0'], 'server');
   api.use('browser-policy@1.0.0', 'server', { weak: true });
   api.use('lmieulet:meteor-coverage@1.1.4 || 2.0.1 || 3.0.0 || 4.1.0', 'client', { weak: true });
 
