eb018b4647cd744f1cda285f78d33dab398f82e5
jankapunkt <jkuester@uni-bremen.de>
2024-06-14 08:38:01 +0200

    publish 3.1.0-rc.1

--- package/package.js
+++ package/package.js
@@ -3,19 +3,19 @@ Package.describe({
   summary: 'Run Meteor package or app tests with Mocha',
   git: 'https://github.com/meteortesting/meteor-mocha.git',
   documentation: '../README.md',
-  version: '3.1.0-rc.0',
+  version: '3.1.0-rc.1',
   testOnly: true,
 });
 
 Package.onUse(function onUse(api) {
-  api.versionsFrom(['2.8.0', '3.0-rc.2'])
+  api.versionsFrom(['2.8.0', '3.0-rc.4'])
   api.use([
-    'meteortesting:mocha-core@8.3.1-rc300.0',
+    'meteortesting:mocha-core@8.3.1-rc300.1',
     'ecmascript'
   ]);
 
   api.use([
-    'meteortesting:browser-tests@1.6.0-rc300.0',
+    'meteortesting:browser-tests@1.6.0-rc300.1',
     'http@1.0.0 || 2.0.0'
   ], 'server');
   api.use('browser-policy', 'server', { weak: true });
