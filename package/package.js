9d231c230d34de742eb2b725cf68c00c8325119c
d.ruggeroni <d.ruggeroni@lectra.com>
2024-07-31 16:56:38 +0200

    changed version

--- package/package.js
+++ package/package.js
@@ -3,23 +3,22 @@ Package.describe({
   summary: 'Run Meteor package or app tests with Mocha',
   git: 'https://github.com/meteortesting/meteor-mocha.git',
   documentation: '../README.md',
-  version: '3.0.0',
+  version: '3.0.1',
   testOnly: true,
 });
 
 Package.onUse(function onUse(api) {
   api.versionsFrom(['2.8.0', '3.0'])
   api.use([
-    'meteortesting:mocha-core@8.2.0',
-    'ecmascript'
+    'meteortesting:mocha-core',
   ]);
 
   api.use([
-    'meteortesting:browser-tests@1.7.0',
+    'meteortesting:browser-tests',
     'http@1.0.0 || 2.0.0'
   ], 'server');
   api.use('browser-policy', 'server', { weak: true });
-  api.use('lmieulet:meteor-coverage@1.1.4 || 2.0.1 || 3.0.0 || 4.1.0', 'client', { weak: true });
+  // api.use('lmieulet:meteor-coverage@1.1.4 || 2.0.1 || 3.0.0 || 4.1.0', 'client', { weak: true });
 
   api.mainModule('client.js', 'client');
   api.mainModule('server.js', 'server');
