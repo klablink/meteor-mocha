043d098d1e052d7cfe1f3b948af3f123bd224501
jankapunkt <jkuester@uni-bremen.de>
2024-08-06 19:59:55 +0200

    build: prepare for new major release of meteor-coverage

--- package/package.js
+++ package/package.js
@@ -19,7 +19,7 @@ Package.onUse(function onUse(api) {
     'http@1.0.0 || 2.0.0'
   ], 'server');
   api.use('browser-policy', 'server', { weak: true });
-  api.use('lmieulet:meteor-coverage@1.1.4 || 2.0.1 || 3.0.0 || 4.3.0', 'client', { weak: true });
+  api.use('lmieulet:meteor-coverage@1.1.4 || 2.0.1 || 3.0.0 || 4.3.0 || 5.0.0', 'client', { weak: true });
 
   api.mainModule('client.js', 'client');
   api.mainModule('server.js', 'server');
